92ec733eaf9e4c4fb4865e4fac4006ad81be2c12
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2024-02-26 18:08:27 +0800

    Add Swagger

--- src/books.ts
+++ src/books.ts
@@ -1,10 +1,30 @@
 import { Elysia } from "elysia";
 
 const books = new Elysia({ prefix: "/books" })
-  .get("/", "list of books")
-  .post("/", "Add Book")
-  .put("/", "Update Book")
-  .get("/:id", ({ params: { id } }) => "viewing book: " + id)
-  .delete("/:id", ({ params: { id } }) => "deleting book: " + id);
+  .get("/", "list of books", {
+    detail: {
+      tags: ["Books"],
+    },
+  })
+  .post("/", "Add Book", {
+    detail: {
+      tags: ["Add Book"],
+    },
+  })
+  .put("/:id", ({ params: { id } }) => "Updating book: " + id, {
+    detail: {
+      tags: ["Update Book"],
+    },
+  })
+  .get("/:id", ({ params: { id } }) => "viewing book: " + id, {
+    detail: {
+      tags: ["View Book"],
+    },
+  })
+  .delete("/:id", ({ params: { id } }) => "deleting book: " + id, {
+    detail: {
+      tags: ["Delete Book"],
+    },
+  });
 
 export default books;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 import { Elysia } from "elysia";
 import books from "./books";
+import { swagger } from "@elysiajs/swagger";
 
-const v1 = new Elysia({ prefix: "/v1" }).use(books);
+const v1 = new Elysia({ prefix: "/v1" })
+  .use(
+    swagger({
+      path: "/swagger",
+      documentation: {
+        tags: [
+          { name: "Books", description: "Getting the list of books" },
+          { name: "Add Book", description: "Adding a book" },
+          { name: "Update Book", description: "Updating a book" },
+          { name: "View Book", description: "Getting a book" },
+          { name: "Delete Book", description: "Deleting a book" },
+        ],
+      },
+    })
+  )
+  .use(books);
 
 const app = new Elysia().use(v1);
 app.get("/v1", () => "Hello Elysia");
